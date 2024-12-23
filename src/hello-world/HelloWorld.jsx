b0759c7b71554e34ca013918792ed12d86e7159d
developer ireporter <wahyusantozo36@gmail.com>
2024-12-23 20:15:47 +0700

    feat:component

---
+++ src/hello-world/HelloWorld.jsx
@@ -0,0 +1,20 @@
+export default function HelloWorld() {
+    return (
+        <>
+            <HeaderHelloWord/>
+            <ParagrafHelloWord/>
+        </>
+    );
+}
+
+function HeaderHelloWord() { 
+    return (
+        <h1>Hello, World!</h1>
+    )
+}
+
+function ParagrafHelloWord() { 
+    return (
+        <p>Build Web React JS, Tutorial from PZN</p>
+    )
+ }
---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // setup multiple file html
+  build: {
+    rollupOptions: {
+      input: {
+        index: "index.html",
+        hello_world: "hello-world.html"
+      }
+    }
+  }
+})
