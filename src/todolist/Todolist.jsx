35cfdbdad294d3de01858ceb48feba67fdecfa1e
wahyuWs <wahyusantozo36@gmail.com>
2025-02-12 20:22:55 +0700

    feat: (13) collection component

--- src/todolist/Todolist.jsx
+++ src/todolist/Todolist.jsx
@@ -1,12 +1,34 @@
 import Todo from "./Todo";
 
 export default function Todolist() {
+    const data = [
+        {
+            id: 0,
+            text: "Learn HTML",
+            isCompleted: true
+        },
+        {
+            id: 1,
+            text: "Learn CSS",
+            isCompleted: true
+        },
+        {
+            id: 2,
+            text: "Learn Javascript",
+            isCompleted: true
+        },
+        {
+            id: 3,
+            text: "Learn React",
+            isCompleted: false
+        },
+    ];
+
     return (
         <ul>
-            <Todo isCompleted={true} text="Learn HTML"/>
-            <Todo isCompleted={true} text="Learn CSS" isDeleted={true}/>
-            <Todo isCompleted={true} text="Learn Javascript"/>
-            <Todo isCompleted={false} text="Learn React"/>
+            {data.map((todo) => (
+                    <Todo key={todo.id} {...todo}/> /*spread*/
+                ))}
         </ul>
     )
 }
