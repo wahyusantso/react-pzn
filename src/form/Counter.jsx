76c0a65508cb2abcb96d7c57c14ba3ce95358085
wahyusantso <wahyusantozo36@gmail.com>
2025-06-17 23:29:19 +0700

    feat (23) State Updates

--- src/form/Counter.jsx
+++ src/form/Counter.jsx
@@ -5,14 +5,21 @@ export default function Counter() {
     let [counter, setCounter] = useState(0);
     
     function handleClick() {
-        setCounter(counter + 1); //ketika ada perubahan state react akan mengirim antrian untuk memicu render ulang. itu sebabnya perlu menggunakan useState menyimpan data sebelumnya.
+        // setCounter(counter + 1); //ketika ada perubahan state react akan mengirim antrian untuk memicu render ulang. itu sebabnya perlu menggunakan useState menyimpan data sebelumnya.
         
         /*
         dalam case snapshot(variabel dalam state), data tidak akan berubah karena state hanya memicu untuk dirender ulang, data baru akan berubah jika render terjadi.
         ketika membuat setCounter sebanyak 3x, bukan berarti react akan render sebanyak 3x, melainkan setCounter terakhir yang akan dirender, ini terjadi karena react menunggu event handler sampai selesai
+        
+        setCounter(counter + 1);
+        setCounter(counter + 1);
         **/
-       setCounter(counter + 1);
-       setCounter(counter + 1);
+
+        //gunakan function didalam setCounter, jika ingin mengubah data berkali-kali dalam 1 event handler. ini bisa terjadi karena data disimpan di state management setCounter.
+        setCounter((c) => c + 1);
+        setCounter((c) => c + 1);
+        setCounter((c) => c + 1);
+       
         console.log(counter);
     }
 
