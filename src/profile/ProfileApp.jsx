7a84ff5777d5eae9e22db11485614af16672c5ba
wahyusantso <wahyusantozo36@gmail.com>
2025-07-21 11:28:06 +0700

    feat (31) Context dan State

--- src/profile/ProfileApp.jsx
+++ src/profile/ProfileApp.jsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import Profile from "./Profile";
 import ProfileAddress from "./ProfileAddress";
 import { ProfileContext } from "./ProfileContext";
+import ProfileForm from "./ProfileForm";
 
 export default function ProfileApp() {
+    /*
+    - Disarankan menggunakan context() ketika memiliki banyak child, jika masih sedikit gunakan Props.
+    - Mengubah nilai Context() hanya bisa dilakukan di class parent dan memerlukan useState prosesnya.
+     */
+    const [name, setName] = useState("Wahyu Santoso");
+
     return (
         <>
-            <ProfileContext.Provider value="Wahyu Santoso">
+            <ProfileContext.Provider value={name}>
                 <h1>Profile App</h1>
+                <ProfileForm name={name} setName={setName}/>
                 <Profile/>
                 <ProfileAddress/>
             </ProfileContext.Provider>
